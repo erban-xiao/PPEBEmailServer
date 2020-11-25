982b295c2ad320f398e69ccd21b88b55e027f8a0
erban <943186182@qq.com>
2020-11-25 22:26:52 +0800

    初始化提交

---
+++ src/main/java/com/ppebemail/PPEBEmailServer/PpebEmailServerApplication.java
@@ -0,0 +1,13 @@
+package com.ppebemail.PPEBEmailServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpebEmailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpebEmailServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppebemail/PPEBEmailServer/PpebEmailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppebemail.PPEBEmailServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpebEmailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
